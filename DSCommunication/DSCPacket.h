24e95054489e209e8c55d22e27e0113fddc1ed0c
Ninokz <2606904530@qq.com>
2024-08-20 19:14:54 +0800

    添加 _WIN32_WINNT=0x0601 宏定义

--- DSCommunication/DSCPacket.h
+++ DSCommunication/DSCPacket.h
@@ -1,9 +1,4 @@
 #pragma once
-#pragma once
-#ifdef _MSC_VER
-#define _WIN32_WINNT 0x0601
-#endif
-
 #include <iostream>
 #include <string>
 #include <vector>
--- DSCommunication/IOServicePool.h
+++ DSCommunication/IOServicePool.h
@@ -1,8 +1,4 @@
 #pragma once
-#ifdef _MSC_VER
-#define _WIN32_WINNT 0x0601
-#endif
-
 #include <vector>
 #include <memory>
 
